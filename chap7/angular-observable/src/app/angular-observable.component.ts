a3cd2689675e6aff507083befc313d9934c5cd43
Mathieu Nayrolles <mathieu.nayrolles@gmail.com>
2016-07-25 09:38:16 -0400

    reformat for chapt base directory + blue print observable

---
+++ chap7/angular-observable/src/app/angular-observable.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'angular-observable-app',
+  templateUrl: 'angular-observable.component.html',
+  styleUrls: ['angular-observable.component.css']
+})
+export class AngularObservableAppComponent {
+  title = 'angular-observable works!';
+}
